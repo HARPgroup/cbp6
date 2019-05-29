95fa571be68a96df8eac041f95960f6e70bc72f9
Robert W. Burgholzer <rburghol@vt.edu>
2019-05-29 14:37:10 -0400

    Added utils directory and readme on basic scripts for using AWS cli utils.

---
+++ utils/wdm_sync/README.sync_all.sh
@@ -0,0 +1,12 @@
+# These are not fully tested commands, but they are a starting point
+# Sample command to rsyn entiure directory tree
+aws s3 sync s3://bluefish-datashare.chesapeakebay.net/source/ /path/to/local/copy
+
+# Sync a single file
+#For a single file however, just the CP command is necessary
+# File name can be omitted, or included, or changed on the destination similar to the linux cp command.
+aws s3 cp s3://bluefish-datashare.chesapeakebay.net/source/FileXYZ.csv /path/to/source2/FileXYZ.csv
+
+
+# R repo for cli tools
+https://github.com/cloudyr/aws.s3
