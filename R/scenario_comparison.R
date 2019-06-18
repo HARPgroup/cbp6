9f02a24b7c536673412e5921aedf7986ea3c1685
Daniel Hildebrand <danielh7@vt.edu>
2019-06-18 10:43:51 -0400

    Added Emma's changes

--- R/scenario_comparison.R
+++
@@ -1,6 +0,0 @@
-scenario.comparison <- function(metrics1, metrics2) {
-  # Calculates percent change for all metrics (mod2-mod1/mod1, since mod1 is considered basis of comparison)
-  
-  # Returns dataframe of pct changes for all metrics
-  return()
-}
