a871b979b60ca272273aeeae1df3877c0f2f71a2
emma aguero <emmaa4@vt.edu>
2019-06-19 13:21:21 -0400

    Comparisons for segments of interest

--- code/cbp6_comparisons.R
+++ code/cbp6_comparisons.R
@@ -21,17 +21,26 @@ basepath.stop <- as.numeric(which(split.location == 'code'))
 container <- paste0(split.location[1:basepath.stop], collapse = "/")
 
 source(paste0(container, "/cbp6_functions.R"))
+source(paste0(container, "/DEQ_Model_vs_USGS_Comparison/code/function_holder.R"))
 
 # Inputs
-riv.seg <- 'JA5_7480_0001'
+riv.seg <- 'PM7_4820_0001'
 #gage.num <- ''
 start.date <- '1984-01-01'
-end.date <- '2005-12-31'
+end.date <- '2000-12-31'
 
-mod.phase1 <- 'p532c-sova'        # should be "p6/p6_gb604/tmp" (phase 6) or "p532c-sova" (phase 5)
-mod.phase2 <- 'p6/p6_gb604/tmp'        # should be "p6/p6_gb604/tmp" (phase 6) or "p532c-sova" (phase 5)
-mod.scenario1 <- 'p532cal_062211' # should be "CFBASE30Y20180615" (phase 6) or "p532cal_062211"
-mod.scenario2 <- 'CFBASE30Y20180615' # should be "CFBASE30Y20180615" (phase 6) or "p532cal_062211"
+# Phase Options
+    # "p532c-sova"        (phase 5)
+    # "p6/p6_gb604/tmp"   (phase 6)
+mod.phase1 <- 'p6/p6_gb604/tmp'      
+mod.phase2 <- 'p6/p6_gb604/tmp'       
+
+# Scenario Options
+    # "p532cal_062211"                (phase 5)
+    # "CFBASE30Y20180615"             (phase 6)
+    # "CBASE1808L55CY55R45P50R45P50Y" (phase 6)
+mod.scenario1 <- 'CBASE1808L55CY55R45P50R45P50Y'
+mod.scenario2 <- 'CFBASE30Y20180615'
 
 # Loading data for models
 data1 <- model.import.data.cfs(riv.seg, mod.phase1, mod.scenario1, start.date, end.date)
@@ -48,15 +57,17 @@ data2_trim <- water.year.trim(data2)
 ## Calculate and Compare Metrics ----------------------------------------------------------------------------------
 
 # Individual calculations for models and gages
-data1_metrics <- metric.compile(data1_trim)
-data2_metrics <- metric.compile(data2_trim)
+data1_metrics <- metrics.calc.all(data1_trim)
+data2_metrics <- metrics.calc.all(data2_trim)
 
 # Metrics for VAHydro
 
 
 
 # Comparison Table
-comparison <- compare.models(data1_metrics, data2_metrics)
+comparison <- metrics.compare(data1_metrics, data2_metrics)
 
-
+# Write csv
+write.csv(comparison, paste0(container,"\\Results\\Tables\\",riv.seg, "_", mod.scenario1, "_vs_", mod.scenario2,
+                         "_Comparison Results.csv"))
 
