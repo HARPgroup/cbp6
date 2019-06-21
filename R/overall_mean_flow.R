60028703f83c62e2c6276bbf697672efea12a4be
Daniel Hildebrand <danielh7@vt.edu>
2019-06-21 14:44:33 -0400

    added overall_mean_flow

---
+++ R/overall_mean_flow.R
@@ -0,0 +1,14 @@
+#' Overall Mean Flow Metric Function
+#' @description Measures the mean flow of the entire timeseries
+#' @param data dataframe of date and flow data taken from import data functions
+#' @return overall mean flow metric
+#' @import lubridate
+#' @export overall_mean_flow
+
+overall_mean_flow <- function(data) {
+  data$year <- year(ymd(data$date))
+  data$month <- month(ymd(data$date))
+  data$day <- day(ymd(data$date))
+  mean.flow <- signif(mean(data$flow), digits=3);
+  return(mean.flow)
+}
