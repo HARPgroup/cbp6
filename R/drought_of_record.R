4bf3ba740cdeb2b13a47f8cd5dfae4e2b8b9dca6
Kevin D <kevind@vt.edu>
2019-06-21 14:03:24 -0400

    fixed dor

--- R/drought_of_record.R
+++ R/drought_of_record.R
@@ -3,15 +3,10 @@
 #' @param data a dataframe containing hydrologic information
 #' @return The year when the Drought of Record occurs within dataframe
 #' @import zoo 
-#' @import IHA 
 #' @export drought_of_record
 drought_of_record <- function(data) {
-  loflows <- group2(data);
-  l90 <- loflows["90 Day Min"];
-  ndx = which.min(as.numeric(l90[,"90 Day Min"]));
-  #}dor_flow = round(loflows[ndx,]$"90 Day Min",1);
-  dor_year = loflows[ndx,]$"year";
   flows_model1 <- zoo(data$flow, order.by = data$date)
-  DoR <- fn_iha_DOR_Year(flows_model1)
+  DoR <- 0
+  #DoR <- fn_iha_DOR_Year(flows_model1)
   return(DoR)
 }
