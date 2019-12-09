63b7da6d0987616e49bc30d92ce35e0cdce05be7
Daniel Hildebrand <danielh7@vt.edu>
2019-12-09 15:46:11 -0500

    bug fixes on dashboard

--- R/vahydro_import_lrseg_all_flows.R
+++ R/vahydro_import_lrseg_all_flows.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 vahydro_import_lrseg_all_flows <- function(lr.seg.hydrocode, run.id, token, site = "http://deq2.bse.vt.edu/d.dh", start.date = '1984-01-01', end.date) {
   #set hydrocode equal to the ith hydrocode in the lrseg list
   hydrocode = lr.seg.hydrocode
@@ -40,7 +42,52 @@ vahydro_import_lrseg_all_flows <- function(lr.seg.hydrocode, run.id, token, site
   
   dat <- as.data.frame(dat)
   
-  dat.trim <- dat[,c(9,146,15:143)]
+  dat.trim <- select(dat,
+                     thisdate, Qout, aop_suro, aop_ifwo, aop_agwo, 
+                     cch_suro, cch_ifwo, cch_agwo, 
+                     cci_suro, cci_ifwo, cci_agwo, 
+                     ccn_suro, ccn_ifwo, ccn_agwo, 
+                     cfr_suro, cfr_ifwo, cfr_agwo, 
+                     cir_suro, cir_ifwo, cir_agwo, 
+                     cmo_suro, cmo_ifwo, cmo_agwo, 
+                     cnr_suro, cnr_ifwo, cnr_agwo, 
+                     ctg_suro, ctg_ifwo, ctg_agwo, 
+                     dbl_suro, dbl_ifwo, dbl_agwo, 
+                     fnp_suro, fnp_ifwo, fnp_agwo, 
+                     for_suro, for_ifwo, for_agwo, 
+                     fsp_suro, fsp_ifwo, fsp_agwo, 
+                     gom_suro, gom_ifwo, gom_agwo, 
+                     gwm_suro, gwm_ifwo, gwm_agwo, 
+                     hfr_suro, hfr_ifwo, hfr_agwo, 
+                     lhy_suro, lhy_ifwo, lhy_agwo, 
+                     mch_suro, mch_ifwo, mch_agwo, 
+                     mci_suro, mci_ifwo, mci_agwo, 
+                     mcn_suro, mcn_ifwo, mcn_agwo, 
+                     mir_suro, mir_ifwo, mir_agwo, 
+                     mnr_suro, mnr_ifwo, mnr_agwo, 
+                     mtg_suro, mtg_ifwo, mtg_agwo, 
+                     nch_suro, nch_ifwo, nch_agwo, 
+                     nci_suro, nci_ifwo, nci_agwo, 
+                     nir_suro, nir_ifwo, nir_agwo, 
+                     nnr_suro, nnr_ifwo, nnr_agwo, 
+                     ntg_suro, ntg_ifwo, ntg_agwo, 
+                     oac_suro, oac_ifwo, oac_agwo, 
+                     ohy_suro, ohy_ifwo, ohy_agwo, 
+                     osp_suro, osp_ifwo, osp_agwo, 
+                     pas_suro, pas_ifwo, pas_agwo, 
+                     sch_suro, sch_ifwo, sch_agwo, 
+                     scl_suro, scl_ifwo, scl_agwo, 
+                     sgg_suro, sgg_ifwo, sgg_agwo, 
+                     sho_suro, sho_ifwo, sho_agwo, 
+                     som_suro, som_ifwo, som_agwo, 
+                     soy_suro, soy_ifwo, soy_agwo, 
+                     stb_suro, stb_ifwo, stb_agwo, 
+                     stf_suro, stf_ifwo, stf_agwo, 
+                     swm_suro, swm_ifwo, swm_agwo, 
+                     wfp_suro, wfp_ifwo, wfp_agwo, 
+                     wto_suro, wto_ifwo, wto_agwo)
+                     
+  #dat.trim <- dat[,c(9,146,15:143)]
   rownames(dat.trim) <- NULL
   
   start.line <- as.numeric(which(dat.trim$thisdate == start.date))
--- code/cbp6_functions.R
+++ code/cbp6_functions.R
@@ -18,6 +18,7 @@ library(ggsn)
 library(sp)
 library(rlist)
 
+
 model_import_data_cfs <- function(riv.seg, mod.phase, mod.scenario, start.date, end.date) {
   # Downloading and exporting hourly model data
   model_hourly <- read.csv(paste0("http://deq2.bse.vt.edu/", mod.phase, "/out/river/", mod.scenario, "/stream/", 
@@ -2157,32 +2158,26 @@ vahydro_import_data_cfs <- function(riv.seg, run.id, token, site = "http://deq2.
   return(dat.trim)
 }
 
-vahydro_import_lrseg_data_cfs <- function(riv.seg, run.id, token, site = "http://deq2.bse.vt.edu/d.dh", start.date = '1984-01-01', end.date) {
-  hydrocode = paste0("cbp6_", riv.seg);
-  ftype = 'cbp6_lrseg'; # nhd_huc8, nhd_huc10, vahydro
+library(dplyr)
+
+vahydro_import_lrseg_all_flows <- function(lr.seg.hydrocode, run.id, token, site = "http://deq2.bse.vt.edu/d.dh", start.date = '1984-01-01', end.date) {
+  #set hydrocode equal to the ith hydrocode in the lrseg list
+  hydrocode = lr.seg.hydrocode
+  
+  #set inputs for lrseg property
+  ftype = 'cbp6_lrseg'
+  bundle = 'landunit'
   inputs <- list (
     hydrocode = hydrocode,
-    bundle = 'landunit',
-    ftype = 'cbp6_lrseg'
+    bundle = bundle,
+    ftype = ftype
   )
   
   #property dataframe returned
   feature = FALSE;
   odata <- getFeature(inputs, token, site, feature);
   
-  # Get data frame for stashing multirun data
-  stash <- data.frame();
-  mostash <- data.frame();
-  tsstash = FALSE;
   featureid <- odata[1,"hydroid"];
-  fname <- as.character(odata[1,]$name );
-  # inputs <- list(
-  #   varkey = "wshed_local_area_sqmi",
-  #   featureid = featureid,
-  #   entity_type = "dh_feature"
-  # )
-  # da <- getProperty(inputs, site, model)
-  
   inputs <- list(
     varkey = "om_class_cbp_eos_file",
     featureid = featureid,
@@ -2190,39 +2185,73 @@ vahydro_import_lrseg_data_cfs <- function(riv.seg, run.id, token, site = "http:/
     propcode = "vahydro-1.0"
   )
   model <- getProperty(inputs, site, model)
-  mid = as.numeric(as.character(model[1,]$pid))
+  
+  fin = as.numeric(as.character(model[1,]$pid))
   inputs <- list(
     varkey = "om_element_connection",
-    featureid = mid,
+    featureid = fin,
     entity_type = "dh_properties"
   )
   prop <- getProperty(inputs, site, prop)
   
-  
-  
   # Manual elid
   elid = as.numeric(as.character(prop[1,]$propvalue))
-  
-  wshed_summary_tbl = data.frame(
-    "Run ID" = character(), 
-    "Segment Name (D. Area)" = character(), 
-    "7Q10/ALF/LF-90" = character(), 
-    "WD (mean/max)" = character(), 
-    stringsAsFactors = FALSE) ;
-  #pander(odata);
-  #pander(odata);
   
   omsite = site <- "http://deq2.bse.vt.edu"
   dat <- fn_get_runfile(elid, run.id, site = omsite,  cached = FALSE);
   
-  dat.date <- as.Date(as.character(dat$thisdate))
-  dat.flow <- as.numeric(dat$Qout)
+  dat <- as.data.frame(dat)
   
-  dat.trim <- data.frame(dat.date, dat.flow, row.names = NULL)
-  colnames(dat.trim) <- c('date','flow')
+  dat.trim <- select(dat,
+                     thisdate, Qout, aop_suro, aop_ifwo, aop_agwo, 
+                     cch_suro, cch_ifwo, cch_agwo, 
+                     cci_suro, cci_ifwo, cci_agwo, 
+                     ccn_suro, ccn_ifwo, ccn_agwo, 
+                     cfr_suro, cfr_ifwo, cfr_agwo, 
+                     cir_suro, cir_ifwo, cir_agwo, 
+                     cmo_suro, cmo_ifwo, cmo_agwo, 
+                     cnr_suro, cnr_ifwo, cnr_agwo, 
+                     ctg_suro, ctg_ifwo, ctg_agwo, 
+                     dbl_suro, dbl_ifwo, dbl_agwo, 
+                     fnp_suro, fnp_ifwo, fnp_agwo, 
+                     for_suro, for_ifwo, for_agwo, 
+                     fsp_suro, fsp_ifwo, fsp_agwo, 
+                     gom_suro, gom_ifwo, gom_agwo, 
+                     gwm_suro, gwm_ifwo, gwm_agwo, 
+                     hfr_suro, hfr_ifwo, hfr_agwo, 
+                     lhy_suro, lhy_ifwo, lhy_agwo, 
+                     mch_suro, mch_ifwo, mch_agwo, 
+                     mci_suro, mci_ifwo, mci_agwo, 
+                     mcn_suro, mcn_ifwo, mcn_agwo, 
+                     mir_suro, mir_ifwo, mir_agwo, 
+                     mnr_suro, mnr_ifwo, mnr_agwo, 
+                     mtg_suro, mtg_ifwo, mtg_agwo, 
+                     nch_suro, nch_ifwo, nch_agwo, 
+                     nci_suro, nci_ifwo, nci_agwo, 
+                     nir_suro, nir_ifwo, nir_agwo, 
+                     nnr_suro, nnr_ifwo, nnr_agwo, 
+                     ntg_suro, ntg_ifwo, ntg_agwo, 
+                     oac_suro, oac_ifwo, oac_agwo, 
+                     ohy_suro, ohy_ifwo, ohy_agwo, 
+                     osp_suro, osp_ifwo, osp_agwo, 
+                     pas_suro, pas_ifwo, pas_agwo, 
+                     sch_suro, sch_ifwo, sch_agwo, 
+                     scl_suro, scl_ifwo, scl_agwo, 
+                     sgg_suro, sgg_ifwo, sgg_agwo, 
+                     sho_suro, sho_ifwo, sho_agwo, 
+                     som_suro, som_ifwo, som_agwo, 
+                     soy_suro, soy_ifwo, soy_agwo, 
+                     stb_suro, stb_ifwo, stb_agwo, 
+                     stf_suro, stf_ifwo, stf_agwo, 
+                     swm_suro, swm_ifwo, swm_agwo, 
+                     wfp_suro, wfp_ifwo, wfp_agwo, 
+                     wto_suro, wto_ifwo, wto_agwo)
   
-  start.line <- as.numeric(which(dat.trim$date == start.date))
-  end.line <- as.numeric(which(dat.trim$date == end.date))
+  #dat.trim <- dat[,c(9,146,15:143)]
+  rownames(dat.trim) <- NULL
+  
+  start.line <- as.numeric(which(dat.trim$thisdate == start.date))
+  end.line <- as.numeric(which(dat.trim$thisdate == end.date))
   dat.trim <- dat.trim[start.line:end.line,]
   
   return(dat.trim)
@@ -3745,58 +3774,6 @@ link.cbp6.lrseg.hydrocodes = function(riv.seg, psk, site, token) {
   cbp6.landunits <- as.vector(land.units$hydrocode[land.units$Bundle..Type == 'Land Unit: cbp6_lrseg'])
   
   return(cbp6.landunits)
-}
-
-vahydro_import_lrseg_all_flows <- function(lr.seg.hydrocode, run.id, token, site = "http://deq2.bse.vt.edu/d.dh", start.date = '1984-01-01', end.date) {
-  #set hydrocode equal to the ith hydrocode in the lrseg list
-  hydrocode = lr.seg.hydrocode
-  
-  #set inputs for lrseg property
-  ftype = 'cbp6_lrseg'
-  bundle = 'landunit'
-  inputs <- list (
-    hydrocode = hydrocode,
-    bundle = bundle,
-    ftype = ftype
-  )
-  
-  #property dataframe returned
-  feature = FALSE;
-  odata <- getFeature(inputs, token, site, feature);
-  
-  featureid <- odata[1,"hydroid"];
-  inputs <- list(
-    varkey = "om_class_cbp_eos_file",
-    featureid = featureid,
-    entity_type = "dh_feature",
-    propcode = "vahydro-1.0"
-  )
-  model <- getProperty(inputs, site, model)
-  
-  fin = as.numeric(as.character(model[1,]$pid))
-  inputs <- list(
-    varkey = "om_element_connection",
-    featureid = fin,
-    entity_type = "dh_properties"
-  )
-  prop <- getProperty(inputs, site, prop)
-  
-  # Manual elid
-  elid = as.numeric(as.character(prop[1,]$propvalue))
-  
-  omsite = site <- "http://deq2.bse.vt.edu"
-  dat <- fn_get_runfile(elid, run.id, site = omsite,  cached = FALSE);
-  
-  dat <- as.data.frame(dat)
-  
-  dat.trim <- dat[,c(9,146,15:143)]
-  rownames(dat.trim) <- NULL
-  
-  start.line <- as.numeric(which(dat.trim$thisdate == start.date))
-  end.line <- as.numeric(which(dat.trim$thisdate == end.date))
-  dat.trim <- dat.trim[start.line:end.line,]
-  
-  return(dat.trim)
 }
 
 link.cbp6.lrseg.hydrocodes = function(riv.seg, psk, site, token) {
